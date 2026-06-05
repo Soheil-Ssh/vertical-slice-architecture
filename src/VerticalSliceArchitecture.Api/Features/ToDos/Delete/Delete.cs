eb4f98757e8a9b66017a0824749acda4916e50f7
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-06-05 23:43:42 +0330

    feat: add delete todo feature

---
+++ src/VerticalSliceArchitecture.Api/Features/ToDos/Delete/Delete.cs
@@ -0,0 +1,36 @@
+﻿using VerticalSliceArchitecture.Api.Entities.ToDo;
+
+namespace VerticalSliceArchitecture.Api.Features.ToDos.Delete;
+
+public static class Delete
+{
+    public sealed record Command(long Id) : IRequest<Result>;
+
+    public class Handler(ApplicationDbContext context) : IRequestHandler<Command, Result>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var todo = await context.ToDos
+                .FirstOrDefaultAsync(t => t.Id == request.Id, cancellationToken);
+            if (todo is null)
+                return ToDoErrors.NotFound;
+
+            context.ToDos.Remove(todo);
+            await context.SaveChangesAsync(cancellationToken);
+            return Result.Success();
+        }
+    }
+
+    public class Endpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("api/tasks/{id:long}", async (long id, ISender sender) =>
+            {
+                var command = new Command(id);
+                var result = await sender.Send(command);
+                return result.ToHttpResult();
+            });
+        }
+    }
+}
