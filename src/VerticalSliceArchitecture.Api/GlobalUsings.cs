7eb36f23b1540bf11dde5a3d9c4ad08440098a56
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-06-05 21:34:15 +0330

    fix: replace auto mapper with maps

--- src/VerticalSliceArchitecture.Api/GlobalUsings.cs
+++ src/VerticalSliceArchitecture.Api/GlobalUsings.cs
@@ -2,3 +2,6 @@
 global using VerticalSliceArchitecture.Api.Data;
 global using FluentValidation;
 global using Carter;
+global using MediatR;
+global using Mapster;
+global using VerticalSliceArchitecture.Api.Common.Result;
