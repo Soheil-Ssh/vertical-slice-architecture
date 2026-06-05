2d4494914fff0e63cbb03a3998f10c0db6e3fcb8
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-06-05 20:29:39 +0330

    feat: add and configure packages

--- src/VerticalSliceArchitecture.Api/GlobalUsings.cs
+++ src/VerticalSliceArchitecture.Api/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using VerticalSliceArchitecture.Api.Entities.Base;
 global using VerticalSliceArchitecture.Api.Data;
+global using FluentValidation;
+global using Carter;
--- src/VerticalSliceArchitecture.Api/Program.cs
+++ src/VerticalSliceArchitecture.Api/Program.cs
@@ -1,13 +1,25 @@
-
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
+// Add application db context
 services.AddDbContext<ApplicationDbContext>(option => 
     option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddControllers();
+
+// Add MediatR
+services.AddMediatR(options =>
+    options.RegisterServicesFromAssemblyContaining(typeof(Program)));
+
+// add Auto mapper profiles
+services.AddAutoMapper(cfg => { }, typeof(Program).Assembly);
+
+// Add Fluent validation
+services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+builder.Services.AddCarter();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -15,12 +27,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapCarter();
 
 app.Run();
